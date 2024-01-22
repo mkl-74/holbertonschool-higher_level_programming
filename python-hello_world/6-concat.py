1754bd7b0df50eaac31e19d456faf10c45b813fe
mkl-74 <mickael1008@gmail.com>
2024-01-22 17:54:44 +0000

    task4

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+
+# #!/usr/bin/python3
+
+# -*- coding: utf-8 -*-
+
+str1 = "Holberton"
+str2 = "School"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"Welcome to {str1 + ' ' + str2}!")
