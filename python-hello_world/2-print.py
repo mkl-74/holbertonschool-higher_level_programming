3f0d10b1cf1eac3c11b2675421c222acf68c9c74
mkl-74 <mickael1008@gmail.com>
2024-01-23 07:37:33 +0000

    task0_modif

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
-# write a script with python that printing "Programming
-# is like building a multilingual puzzle"
-
 print("\"Programming is like building a multilingual puzzle")
