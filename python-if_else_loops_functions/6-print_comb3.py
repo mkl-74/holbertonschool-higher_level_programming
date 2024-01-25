2757ea390a09e72edac75fb8211df3583946bf66
mkl-74 <mickael1008@gmail.com>
2024-01-25 18:17:32 +0000

    task6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,5 @@ for nbr1 in range(0, 9):
     for nbr2 in range(1, 10):
         if nbr1 == nbr2:
             continue
-        print(f"{nbr1}{nbr2},", end=' ')
+        if nbr1 < nbr2:
+            print(f"{nbr1}{nbr2},", end=' ')
