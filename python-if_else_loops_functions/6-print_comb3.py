fe38090b03c5ae240ef70a16678c46615e553794
mkl-74 <mickael1008@gmail.com>
2024-01-25 18:12:49 +0000

    task6-fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
-for nbr1 in range(0, 10):
-    for nbr2 in range(0, 10):
+for nbr1 in range(0, 9):
+    for nbr2 in range(1, 10):
+        if nbr1 == nbr2:
+            continue
         print(f"{nbr1}{nbr2},", end=' ')
