2d73ba959f24464b5eb05d021123c57a35593218
mkl-74 <mickael1008@gmail.com>
2024-01-25 18:35:25 +0000

    task6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,14 @@ for nbr1 in range(0, 9):
         if nbr1 == nbr2:
             continue
         if nbr1 < nbr2:
-            print("{}{},".format(nbr1, nbr2), end=' ')
+            if nbr1 != 8 or nbr2 != 9:
+                print("{:d}{:d},".format(nbr1, nbr2), end=' ')
+            else:
+                print("{:d}{:d}".format(nbr1, nbr2))
+
+#for nbr1 in range(0, 9):
+#    for nbr2 in range(1, 10):
+#        if nbr1 == nbr2:
+#            continue
+#        if nbr1 < nbr2:
+#            print("{}{},".format(nbr1, nbr2), end=' ')
