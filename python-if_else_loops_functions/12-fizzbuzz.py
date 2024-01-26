696869c41af60f300882222155fd32ece6828aeb
mkl-74 <mickael1008@gmail.com>
2024-01-26 13:08:45 +0000

    task12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+
+    for nbr in range(1, 101):
+        if nbr % 3 == 0:
+            print("Fizz")
+        elif nbr % 5 == 0:
+            print("Buzz")
+        elif nbr % 3 == 0 and i % 5 == 0:
+            print("Fizz Buzz")
+        else:
+            print(nbr)
