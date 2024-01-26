df2cd27ae22088839cfbe7756cd32e379180c974
mkl-74 <mickael1008@gmail.com>
2024-01-26 21:47:35 +0000

    task2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    arg_count = len(sys.argv) - 1  # Exclure le nom du script lui-même
+
+    if arg_count == 0:
+        print(f"{arg_count} arguments.")
+    elif arg_count == 1:
+        print(f"{arg_count} argument:")
+    else:
+        print(f"{arg_count} arguments:")
+
+    for i in range(1, len(sys.argv)):
+        print(f"{i}: {sys.argv[i]}")
+
+if __name__ == "__main__":
+    main()
