33e387eda2fed63812db30494c766f14dca1fd44
mkl-74 <mickael1008@gmail.com>
2024-01-28 15:20:17 +0000

    task4

---
+++ 4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#prints all the names defined by the compiled module hidden_4.pyc
+
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_':
+            print("{}".format(name))
+            
