89c2b13e741523b9f30b589cb6d65303a8f7c884
mkl-74 <mickael1008@gmail.com>
2024-01-28 15:22:46 +0000

    task3

--- 3-infinite_add.py
+++ 3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 #  the result of the addition of all arguments
+
 if __name__ == "__main__":
     from sys import argv
     sum = 0
     for count in range(1, len(argv)):
         sum += int(argv[count])
     print("{}".format(sum))
-    
