32fcf27a912229c1caf5a18d97ee1bf1f0f69913
mkl-74 <mickael1008@gmail.com>
2024-01-30 09:45:32 +0000

    task6-modif

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,6 @@
 def print_matrix_integer(matrix=[[]]):
 
     transposed = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+
     for row in transposed:
-        for elem in row:
-            print(elem, end=" ")
-        print()
+        print(" ".join("{:2}".format(elem) for elem in row))
