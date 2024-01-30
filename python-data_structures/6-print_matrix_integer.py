c50c2a7f1b0d791d579184a73345b834bd18ee25
mkl-74 <mickael1008@gmail.com>
2024-01-30 09:40:54 +0000

    task6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# prints a matrix of integers
+
+def print_matrix_integer(matrix=[[]]):
+
+    transposed = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+    for row in transposed:
+        for elem in row:
+            print(elem, end=" ")
+        print()
