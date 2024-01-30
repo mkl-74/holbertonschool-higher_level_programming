851522ba1b741f8a53e076562cb19b27f98d872d
mkl-74 <mickael1008@gmail.com>
2024-01-30 09:56:50 +0000

    task6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,7 @@
 
 # prints a matrix of integers
 
-
 def print_matrix_integer(matrix=[[]]):
+
     for row in matrix:
-        formatted_row = ""
-        for num in row:
-            formatted_row += "{:2} ".format(num)
-        print(formatted_row.rstrip())
+        print(" ".join("{:d}".format(num) for num in row))
