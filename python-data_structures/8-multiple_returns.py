af02c6d6aba6dc9cca8471433f850511adc7a1b7
mkl-74 <mickael1008@gmail.com>
2024-01-31 15:20:26 +0000

    task8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if sentence and sentence[0].isalpha():
-        return len(sentence), f"First character: {sentence[0]}"
+    if sentence:
+        if 'a' <= sentence[0].lower() <= 'z':
+            return len(sentence), f"First character: {sentence[0]}"
+        else:
+            return len(sentence), "Non-alphabetic first character"
     else:
-        return 0, "Empty string or non-alphabetic first character"
+        return 0, "Empty string"
