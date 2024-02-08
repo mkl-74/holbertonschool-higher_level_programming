bee3ebaac4f3afd3f481556ae179585db83fedc0
mkl-74 <mickael1008@gmail.com>
2024-02-08 08:47:01 +0000

    task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 
-""" 2-square.py : Write a class Square that defines a square by:(based on 1-square.py)
-
-"""
+""" 2-square.py : Write a class Square that defines a square by:
+(based on 1-square.py)"""
 
 class Square:
+    """Write a class Square that defines a square by: (based on 1-square.py)"""
     def __init__(self, size=0):
         self.__size = size
         if not isinstance(size, int):
