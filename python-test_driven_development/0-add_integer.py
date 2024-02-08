7133db5e2d71f67f8d1fe0666a770a028d892b31
mkl-74 <mickael1008@gmail.com>
2024-02-08 14:47:24 +0000

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,7 +2,9 @@
 
 
 
+
 def add_integer(a, b=98):
+    """Write a function that adds 2 integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
@@ -12,4 +14,3 @@ def add_integer(a, b=98):
 if __name__ == "__main__":
     import doctest
     doctest.testfile("0-add_integer.txt")
-    
