4aee7e3a21546642204c53b54dc41bd5467cf4ab
mkl-74 <mickael1008@gmail.com>
2024-02-08 08:42:48 +0000

    task1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
-    """1-square.py : Write a class Square that defines a square by: (based on 0-square.py)"""
+    """1-square.py : Write a class Square that defines a square by:
+    (based on 0-square.py)"""
     def __init__(self, size):
         self.__size = size
