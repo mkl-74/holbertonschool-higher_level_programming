13de86d41f3ea6e2600ff1fa436f02d07ecd5a66
mkl-74 <mickael1008@gmail.com>
2024-02-09 13:46:34 +0000

    task5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 
 
+def text_indentation(text):
+    """Print a text with two space line when meet '.' or ':' or '?'"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".:?":
+            print("\n" * 2, end="")
+            while i + 1 < len(text) and text[i + 1] == " ":
+                i += 1
+        i += 1
+
