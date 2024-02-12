0329b01a4f830644152cc7a37028c4fad2e56c28
mkl-74 <129300685+mkl-74@users.noreply.github.com>
2024-02-12 14:59:49 +0000

    all task-fix probleme git

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Rectangle class."""
+
+
+class Rectangle:
+    """Create an empty Rectangle Object."""
+    pass
---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)"""
+
+
+class Rectangle:
+    """Defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initializes a Rectangle instance."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves the width of a Rectangle instance."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of a Rectangle instance."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the height of a Rectangle instance."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of a Rectangle instance."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Returns a string representation of the rectangle with #."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        lines = ["#" * self.width for _ in range(self.height)]
+        rectangle_str = "\n".join(lines)
+        return rectangle_str
