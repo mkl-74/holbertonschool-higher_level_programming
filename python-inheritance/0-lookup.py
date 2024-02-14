fe7f9e865e399192bc3fa1b3a78e99d251c1dd05
mkl-74 <129300685+mkl-74@users.noreply.github.com>
2024-02-14 15:46:12 +0000

    task0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 
+"""return list of attributs and method of an object"""
+  
+  
 def lookup(obj):
-    """return list of attributs and method of an object"""
+    """
+    Args :
+    obj : object whose attributes and methods are listed
+    rerturn : list content the name of  attributs and method
+    """
     return dir(obj)
