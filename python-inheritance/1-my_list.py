64f3606f02c993cc491fdd58b753ddce323ead27
mkl-74 <129300685+mkl-74@users.noreply.github.com>
2024-02-15 10:47:26 +0000

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,11 +6,7 @@
 
 class MyList(list):
     """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
 
     def print_sorted(self):
         """prints the sorted list"""
         print(sorted(self))
-        
