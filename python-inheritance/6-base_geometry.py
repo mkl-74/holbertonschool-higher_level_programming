033c106ab6fe7fca5e3853d284052c6aec2c359c
mkl-74 <mickael1008@gmail.com>
2024-02-16 08:59:45 +0000

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""write a empty class"""
+
+class BaseGeometry:
+    """create a empty class"""
+    def area(self):
+        """raise excerption"""
+        raise Exception ("area() is not implemented")
