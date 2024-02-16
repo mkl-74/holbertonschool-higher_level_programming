5fca75852ca34ab2be2ad321afc1498b12048382
mkl-74 <mickael1008@gmail.com>
2024-02-16 09:28:13 +0000

    task7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""write a empty class"""
+
+
+class BaseGeometry:
+    """create a empty class"""
+    def area(self):
+        """raise excerption"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """Validate or raise exception"""
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+        return value
