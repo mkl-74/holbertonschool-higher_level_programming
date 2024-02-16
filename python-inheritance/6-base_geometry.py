f708b80f7f640b8edc35cf4255f1e7bf98a0f890
mkl-74 <mickael1008@gmail.com>
2024-02-16 09:03:45 +0000

    task6-Fix space

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """write a empty class"""
 
+
 class BaseGeometry:
     """create a empty class"""
     def area(self):
         """raise excerption"""
-        raise Exception ("area() is not implemented")
+        raise Exception("area() is not implemented")
