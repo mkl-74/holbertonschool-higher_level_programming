059256af6bba91ed6b047b439f84dbda7fe7364a
mkl-74 <mickael1008@gmail.com>
2024-02-17 09:37:03 +0000

    task10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""write a class square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
