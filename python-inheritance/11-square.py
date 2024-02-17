9992963a7a9bc72651ec2ab8f780e44766c04bd7
mkl-74 <mickael1008@gmail.com>
2024-02-17 12:41:20 +0000

    task11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""write a new class square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Initialized new class square"""
+    def __init__(self, size):
+        
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """print"""
+        return '[Square] {:d} / {:d}'.format(self._Rectangle__width, self._Rectangle__height)
