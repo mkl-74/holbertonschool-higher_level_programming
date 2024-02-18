3db73f8810a56e4562948cdd41144f8813aee904
mkl-74 <mickael1008@gmail.com>
2024-02-18 15:05:23 +0000

    task0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""function that read a text file"""
+
+
+def read_file(filename="my_file_0.txt"):
+    """
+    Read the files "my_file_0
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        file_content = f.read()
+        print(file_content)
+
+    f.closed
