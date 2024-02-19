53d6e5607216147836fbc5ded09c6d4183be0f0e
mkl-74 <mickael1008@gmail.com>
2024-02-19 12:22:28 +0000

    task4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -2,14 +2,12 @@
 """fonction that return an object represented by a json string"""
 import json
 
-    
+
 def from_json_string(my_str):
     """_summary_
 
     Args:
-        my_str (obj) :
-        
-
+    my_str (obj) :
     Returns:
         _type_: _description_
     """
