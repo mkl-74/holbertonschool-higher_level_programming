403f80bdd30b9019cf60d8f656bc0e0ed094f78e
mkl-74 <mickael1008@gmail.com>
2024-02-19 09:40:38 +0000

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     """
     with open(filename, 'r', encoding="utf-8") as f:
         file_content = f.read()
-        print(file_content)
+        print(file_content, end="")
