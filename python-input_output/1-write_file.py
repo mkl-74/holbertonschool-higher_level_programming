15552bcbf5a1ec8905f07d29156c7e8e9c54fe09
mkl-74 <mickael1008@gmail.com>
2024-02-19 10:23:55 +0000

    task1-del space

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,7 +7,7 @@ def write_file(filename="", text=""):
     Args :
     filename = The name of the file to write
     text = the text written in to the file
-    
+
     Return = the number of characters written to the file
     """
     with open(filename, 'w', encoding='utf-8') as f:
