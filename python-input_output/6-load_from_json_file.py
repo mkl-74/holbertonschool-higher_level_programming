1d580e3b38b20f328def7cc7caf222c5d1863604
mkl-74 <mickael1008@gmail.com>
2024-02-19 15:09:54 +0000

    taqsk6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,13 +6,7 @@ import json
 
 
 def load_from_json_file(filename):
-    """_summary_
-
-    Args:
-        filename (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        return json.loads(file)
+    """Create a python object"""
+    with open(filename) as f:
+        return json.load(f)
+    
