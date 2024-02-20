194b92f0033cfe38ea5a1e8cc6de596a88192904
mkl-74 <mickael1008@gmail.com>
2024-02-20 09:00:59 +0000

    task9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,10 +4,12 @@
 
 class Student:
     def __init__(self, first_name, last_name, age):
+        """initialized a class Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """that retrieves a dictionary representation of a Student"""
         return self.__dict__
     
