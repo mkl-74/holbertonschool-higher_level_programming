a8b8da52bbc53ff83f25802cef0af98663730de4
mkl-74 <mickael1008@gmail.com>
2024-02-22 12:52:27 +0000

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -35,3 +35,33 @@ class Rectangle(Base):
         if not isinstance(value, (int, float)) or value <= 0:
             raise ValueError("Height must be a positive number")
         self.__height = value
+
+    @property
+    def x(self):
+        """allow acces to the value of x"""
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        """Setter method for x-coordinate."""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
+        
+    @property
+    def y(self):
+        """allow acces to the value of y"""
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        """Setter method for y-coordinate."""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -8,19 +8,28 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
 
-    def setup(self):
+    def setUp(self):
         """Reset base"""
         Base.__nb_objects = 0
     
-    def test_nbr_id(self):
-        """test nbr_id"""
+    def test_without_id(self):
+        """test without id"""
         r1 = Rectangle(10, 2)
         r2 = Rectangle(2, 10)
         r3 = Rectangle(8, 10)
         self.assertEqual(r1.id, 1)
         self.assertEqual(r2.id, 2)
         self.assertEqual(r3.id, 3)
-        
+
+    def test_with_id(self):
+        """test with id"""
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r3.id, 12)
+
         
     
     
