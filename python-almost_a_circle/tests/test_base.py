2d867ad9151e2f5ab54fd1596e1ddcd57cf78227
mkl-74 <mickael1008@gmail.com>
2024-02-22 10:41:51 +0000

    test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -6,40 +6,34 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
 
+    def setup(self):
+        """Reset base"""
+        Base.__nb_objects = 0
 
     def test_base_class(self):
         # Creating objects with unique identifiers
         b1 = Base()
         b2 = Base()
         b3 = Base()
-        self.assertEqual(b1.id == 1)
-        self.assertEqual(b2.id == 2)
-        self.assertEqual(b3.id == 3)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
 
     def test_spe_id(self):
         # Creating an object with a specific identifier
         b4 = Base(12)
-        self.assertEqual(b4.id == 12)
+        self.assertEqual(b4.id, 12)
 
     def test_incr_id(self):
         # Automatic increment of identifier
         b5 = Base()
-        self.assertEqual(b5.id == 4)
+        self.assertEqual(b5.id, 4)
 
     def test_neg_id(self):
         # Negative identifier
         b6 = Base(-5)
-        self.assertEqual(b6.id == -5)
+        self.assertEqual(b6.id, -5)
 
-    def test_str_id(self):
-        # Identifier as a string
-        b7 = Base("abc")
-        self.assertIsInstance(b7.id == 5)
-
-    def test_add(self):
-        # Additional test : Check if __nb_objects is correctly updated
-        self.assertEqual(Base.__nb_objects == 5)
 
     if __name__ == "__main__":
-        test_base_class()
-        print("All tests passed!")
+        unittest.main()
