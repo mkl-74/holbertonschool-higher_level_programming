fee0433d7e21b0740e89dd3334c2f12f51a1642e
mkl-74 <mickael1008@gmail.com>
2024-02-22 11:44:15 +0000

    test rectangle

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -5,4 +5,10 @@ import unittest
 from models.rectangle import Rectangle
 
 
-class TestRectangle(unittest.TestCase)
+class TestRectangle(unittest.TestCase):
+    
+    def test_int_pos(self):
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id)
+        
+        
