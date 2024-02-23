0480ac6c3516a70ab9cf1e3fe60b93dce6bc1e57
mkl-74 <mickael1008@gmail.com>
2024-02-23 15:06:31 +0000

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,19 +2,11 @@
 """Create a class Square"""
 
 
-class Square(Rectangle):
-    def __init__(self, size):
+from models.rectangle import rectangle
+
+
+class Square(rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
         """Initialize """
-        super().__init__(size, size)
-        
-    def update(self, *args, **kwargs):
-        if args:
-            if len(args) >= 1:
-                self.width = args[0]
-                self.height = args[0]
-            if len(args) >= 2:
-                self.__id = args[1]
-        if kwargs:
-            self.width = kwargs.get('width', self.width)
-            self.height = kwargs.get('height', self.height)
-            self.__id = kwargs.get('id', self.__id)
+        super().__init__(size, size, x, y, id)
+        self.size = size
