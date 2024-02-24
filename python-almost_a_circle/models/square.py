b2012d91fcaa9fa94d5f6c42f2650636e28dc5a0
mkl-74 <mickael1008@gmail.com>
2024-02-24 21:59:36 +0000

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,8 +4,12 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Create class square that inherit of class rectangle"""
+    """Create class square that inherit from class rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize """
         super().__init__(size, size, x, y, id)
         self.size = size
+
+    def __str__(self):
+        return "[square], ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.size)
