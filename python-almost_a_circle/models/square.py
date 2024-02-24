d482e2c30e7c9d503d7e026a6157d0ab9b2bf995
mkl-74 <mickael1008@gmail.com>
2024-02-24 09:19:07 +0000

    square-comment

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ from models.rectangle import rectangle
 
 
 class Square(rectangle):
+    """Create class square that inherit of class rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize """
         super().__init__(self.width, self.height, x, y, id)
