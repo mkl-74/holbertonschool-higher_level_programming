639ca858a189a4deb933e4c12e43138bed88fd66
mkl-74 <mickael1008@gmail.com>
2024-02-24 09:33:16 +0000

    fix - square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,12 +2,12 @@
 """Create a class Square"""
 
 
-from models.rectangle import rectangle
+from models.rectangle import Rectangle
 
 
-class Square(rectangle):
+class Square(Rectangle):
     """Create class square that inherit of class rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize """
-        super().__init__(self.width, self.height, x, y, id)
+        super().__init__(size, size, x, y, id)
         self.size = size
