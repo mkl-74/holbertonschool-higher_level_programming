209d69c75f882fb1bc3f01d291c9bdb72e9f2fe0
mkl-74 <mickael1008@gmail.com>
2024-02-25 13:19:55 +0000

    rect

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,8 +97,3 @@ class Rectangle(Base):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
-
-    def to_dictionary(self):
-        """return dict"""
-        return {'x': self.x, 'y': self.y, 'id': self.id, 'height': self.height,
-                'width': self.width}
