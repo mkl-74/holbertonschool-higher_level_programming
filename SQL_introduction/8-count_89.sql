26b0c9794cea31c9decf4c8089bec7d2476d06d1
mkl-74 <mickael1008@gmail.com>
2024-03-19 14:04:43 +0000

    task8

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- displays the number of records with id = 89 in the firdst_table
-SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
+SELECT COUNT(*) first_table FROM hbtn_0c_0.first_table WHERE id = 89;
