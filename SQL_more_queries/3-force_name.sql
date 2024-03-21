168da307483a4de613980f07bb7221ef728a901c
mkl-74 <mickael1008@gmail.com>
2024-03-21 10:12:43 +0000

    task3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- Create a table force_name
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_2'.'force_name' (Id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS 'force_name' (Id INT, name VARCHAR(256) NOT NULL);
