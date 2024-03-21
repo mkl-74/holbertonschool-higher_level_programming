93d3749230e70b80e5ede3babdeed6ec5c52ab54
mkl-74 <mickael1008@gmail.com>
2024-03-21 10:11:52 +0000

    task3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,2 @@
 -- Create a table force_name
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_2'.'force_name' (Id INT, name VARCHAR(256));
-GRANT SELECT ON 'hbtn_0d_2'.'force_name' TO 'user_0d_2'@'localhost';
+CREATE TABLE IF NOT EXISTS 'hbtn_0d_2'.'force_name' (Id INT, name VARCHAR(256) NOT NULL);
