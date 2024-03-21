8919e2a2543ac35d80c956ad145c4b37fb67c1ac
mkl-74 <mickael1008@gmail.com>
2024-03-21 14:48:25 +0000

    task9

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Displays lists all cities contained the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states ON states.id = cities.state_id
+ORDER BY cities.id;
