ecaf6fad453dd44be795f5828ee6197f87977853
mkl-74 <mickael1008@gmail.com>
2024-03-21 09:31:19 +0000

    task1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- Create the MYSQL server user user_0d_1
-CREATE user 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE user IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
