f5b7db30b83628752c4c423941bc9c8c5779d657
mkl-74 <mickael1008@gmail.com>
2024-03-25 10:08:03 +0000

    task3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,28 +1,31 @@
-
 #!/usr/bin/python3
-"""
-    Get the states begin by user input of a data base avoiding SQL injection
-"""
+"""Lists states"""
+
 import MySQLdb
 from sys import argv
 
-
-def main():
-    """Only executes when is not imported"""
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1],
-                         port=3306,
-                         passwd=argv[2],
-                         db=argv[3])
-    c = db.cursor()
-    command = """SELECT *
-                 FROM states
-                 WHERE BINARY states.name = %s
-                 ORDER BY states.id ASC"""
-    numrows = c.execute(command, (argv[4],))
-    states = c.fetchall()
-    for idstate in states:
-        print(idstate)
-
 if __name__ == "__main__":
-    main()
+    # Check if the correct number of arguments is provided
+    if len(argv) != 5:
+        print("Usage: {} username password database_name state_name".format(argv[0]))
+        exit(1)
+
+    # Connect to the MySQL database
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+
+    # Prepare the SQL query with a parameter placeholder
+    query = "SELECT * FROM states WHERE name=%s ORDER BY states.id ASC"
+
+    # Execute the query with the parameter value passed as a tuple
+    cur.execute(query, (argv[4],))
+
+    # Fetch and display the results
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    # Close the cursor and the database connection
+    cur.close()
+    conn.close()
