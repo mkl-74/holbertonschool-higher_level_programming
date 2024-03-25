7d51d9eda0bad6a1eaac02814e5f7a0b0ca2c396
mkl-74 <mickael1008@gmail.com>
2024-03-25 10:00:31 +0000

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-"""Lists states"""
-
+"""
+    Get the states begin by user input of a data base
+"""
 import MySQLdb
 from sys import argv
 
+
+def main():
+    """Only executes when is not imported"""
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         port=3306,
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    command = """SELECT *
+                 FROM states
+                 WHERE BINARY states.name = '{}'
+                 ORDER BY states.id ASC"""
+    command = command.format(argv[4])
+    numrows = c.execute(command)
+    states = c.fetchall()
+    for idstate in states:
+        print(idstate)
+
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    query = """
-SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
-    query = query.format(argv[4])
-    cur.execute(query)
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    main()
